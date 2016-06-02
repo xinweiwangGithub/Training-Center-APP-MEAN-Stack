8c5de64ff5931754253dec213bd7ebd20667c68b
Xinwei Wang <xinweiwa@usc.edu>
2016-06-02 13:07:24 -0700

    milestone

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.get('*', function(req, res) {
+	res.render('index');
+});
+
+var port = 3333;
+app.listen(port);
+console.log('listening to port: ' + port);
+
+
+
+
+
+
